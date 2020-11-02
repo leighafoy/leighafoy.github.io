37c47ecd851e6f3cc3ed30a0bf3cbb961a68916f
leighafoy <73842566+leighafoy@users.noreply.github.com>
2020-11-02 11:12:15 -0500

    Lab Test Commit

---
+++ facility3/tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://KdqHBAaVJlAVwT4aSIazNagJcveAVZOy_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "RecTrac Lab Chapter 7: Facility Tab Processing"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": "&lt;p&gt;&lt;span style=&#34;color: rgb(49, 53, 55);&#34;&gt;&lt;span style=&#34;font-size: 17px;&#34;&gt;Welcome to the RecTrac Lab!&amp;nbsp;&lt;/span&gt;&lt;/span&gt;&lt;span style=&#34;color: rgb(0, 0, 0);&#34;&gt;&lt;span style=&#34;font-size: 17px;&#34;&gt;&amp;nbsp;&lt;/span&gt;&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;br&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&#34;color: rgb(49, 53, 55);&#34;&gt;RecTrac&amp;rsquo;s Facility module manages the schedules of your organization&amp;#39;s venues. &amp;nbsp;Meeting rooms, pavilions, gymnasiums, parks - whatever your space, you can reserve it with the Facility module.&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&#34;color: rgb(49, 53, 55);&#34;&gt;&lt;br&gt;&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&#34;color: rgb(49, 53, 55);&#34;&gt;This course explores how to make, change, and cancel facility reservations for your customers using the Facility and Purchase History tabs in Global Sales.&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;br&gt;&lt;/p&gt;&lt;p&gt;&lt;strong&gt;&lt;em&gt;&lt;span style=&#34;color: rgb(225, 49, 97);&#34;&gt;Before you proceed:&lt;/span&gt;&lt;/em&gt;&lt;/strong&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&#34;color: rgb(49, 53, 55);&#34;&gt;This course builds upon content found in previous chapters of the RecTrac Lab. &amp;nbsp;We recommend you complete&amp;nbsp;&lt;/span&gt;&lt;span style=&#34;color: rgb(6, 92, 142);&#34;&gt;&lt;strong&gt;RecTrac Lab Chapter 2: Global Sales&lt;/strong&gt;&lt;/span&gt;&lt;span style=&#34;color: rgb(49, 53, 55);&#34;&gt;&amp;nbsp;and&amp;nbsp;&lt;/span&gt;&lt;span style=&#34;color: rgb(6, 92, 142);&#34;&gt;&lt;strong&gt;RecTrac Lab Chapter 3: Purchase History&lt;/strong&gt;&lt;/span&gt;&lt;span style=&#34;color: rgb(49, 53, 55);&#34;&gt;&amp;nbsp;before getting started.&lt;/span&gt;&lt;/p&gt;"
+};
